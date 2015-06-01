6bb937a980ea5ed4ecaeebbfe796295e412e3ca1
Matt <matthewmorgante@gmail.com>
2015-06-01 15:01:43 -0500

    operators

---
+++ operators.js
@@ -0,0 +1,14 @@
+// operators.js
+
+// == means equal in value 
+// === means equal in value and datatype 
+
+// if the incrementor is added before the variable 
+// i.e. a = 5, ++a; prints 6, 
+// OR a = 5, a++; prints 5 but now the value of a is 6 
+
+// How to determine somethings type: 
+
+var d = "7"; 
+typeof d; 
+--> "string"
