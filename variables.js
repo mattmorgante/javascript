d8642c89b8c1bfbcab72814a0a805c4a0a8a9366
Matt <matthewmorgante@gmail.com>
2015-06-01 14:35:08 -0500

    initial commit

---
+++ variables.js
@@ -0,0 +1,12 @@
+// there are three types in javascript 
+// variables can store text values and variables 
+
+var a = 12 
+var z = "some random text"
+var y = "javascript is awesome"
+
+z + y; 
+
+// types: numbers, strings, and booleans 
+
+a === 12 
