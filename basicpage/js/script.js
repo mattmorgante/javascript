5ae61eb6ca468f688f86fad91625cba126f32f20
Matt <matthewmorgante@gmail.com>
2015-06-05 12:33:18 -0500

    loops

---
+++ basicpage/js/script.js
@@ -0,0 +1,5 @@
+for(var i=0; i<10001; i++){ 
+  if(i%3 === 0){
+  document.write(i + '</br>');
+  }
+}
---
+++ while/js/script.js
@@ -0,0 +1,14 @@
+var i = 0; 
+// while(i<100){
+//   if(i%3 == 0){ 
+//     document.write(i + '<br>');
+//   }
+//   i++; 
+// }
+
+do{ 
+// some action on this code
+// always goes at least once 
+  document.write('hello');
+  i++; 
+}while(i<3); 
