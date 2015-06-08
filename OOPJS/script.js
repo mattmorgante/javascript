6c49ecfe038c65523dda4316158dd914a41a4d15
Matt <matthewmorgante@gmail.com>
2015-06-08 17:45:16 -0500

    animation setup

--- OOPJS/script.js
+++ OOPJS/script.js
@@ -1,12 +1,31 @@
-var car = {
-  'make': 'toyota',
-  'model' : 'corolla',
-  'options': {
-    'fourwheel' : true,
-    'heatedseating' : [true, false]
-  }
-}; // object literal (faster in execution)
+// var car = {
+//   'make': 'toyota',
+//   'model' : 'corolla',
+//   'options': {
+//     'fourwheel' : true,
+//     'heatedseating' : [true, false]
+//   }
+// }; // object literal (faster in execution)
 
-document.write(car.make);
-document.write(car.model);
-document.write(car['options']['fourwheel']);
+// document.write(car.make);
+// document.write(car.model);
+// document.write(car['options']['fourwheel']);
+
+// FUNCTIONS 
+
+var myfunc = function(a, b){ 
+  // a group of actions that happen 
+  var c = a + b 
+  console.log(c)
+  return c;
+}
+
+document.write(myfunc(2, 5));
+
+// Alternative syntax 
+// function myfunc1() { 
+
+// }
+
+document.write(); 
+console.log(); 
